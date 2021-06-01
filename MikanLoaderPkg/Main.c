7f67846823943396dc408a27bac5ef9fe00a1cb1
oriaca <oriaca372m@outlook.jp>
2021-06-01 23:18:59 +0900

    ローダーがある程度まともにEFIを読み込むように

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,4 +1,5 @@
 #include <Guid/FileInfo.h>
+#include <Library/BaseMemoryLib.h>
 #include <Library/MemoryAllocationLib.h>
 #include <Library/PrintLib.h>
 #include <Library/UefiBootServicesTableLib.h>
@@ -8,6 +9,8 @@
 #include <Protocol/LoadedImage.h>
 #include <Protocol/SimpleFileSystem.h>
 #include <Uefi.h>
+
+#include <elf.h>
 
 #include "Protocol/GraphicsOutput.h"
 #include "frame_buffer_config.h"
@@ -204,6 +207,36 @@ static void DrawPixels(EFI_GRAPHICS_OUTPUT_PROTOCOL* gop) {
 	}
 }
 
+static void CalcLoadAddressRange(Elf64_Ehdr* ehdr, UINT64* first_out, UINT64* last_out) {
+	Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+	UINT64 first = MAX_UINT64;
+	UINT64 last = 0;
+
+	for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
+		if (phdr[i].p_type == PT_LOAD) {
+			first = MIN(first, phdr[i].p_vaddr);
+			last = MAX(last, phdr[i].p_vaddr + phdr[i].p_memsz);
+		}
+	}
+
+	*first_out = first;
+	*last_out = last;
+}
+
+static void CopyLoadSegments(Elf64_Ehdr* ehdr) {
+	Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+
+	for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
+		if (phdr[i].p_type == PT_LOAD) {
+			UINT64 segm_in_file = (UINT64)ehdr + phdr[i].p_offset;
+			CopyMem((VOID*)phdr[i].p_vaddr, (VOID*)segm_in_file, phdr[i].p_filesz);
+
+			UINTN remain_bytes = phdr[i].p_memsz - phdr[i].p_filesz;
+			SetMem((VOID*)(phdr[i].p_vaddr + phdr[i].p_filesz), remain_bytes, 0);
+		}
+	}
+}
+
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table) {
 	Print(u"Hello, world from Arch Linux!\n");
 
@@ -270,7 +303,6 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_tab
 		Halt();
 	}
 
-	// カーネルの読み込み
 	EFI_FILE_PROTOCOL* kernel_file;
 	status = root_dir->Open(root_dir, &kernel_file, u"\\kernel.elf", EFI_FILE_MODE_READ, 0);
 	if (EFI_ERROR(status)) {
@@ -289,20 +321,38 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_tab
 	EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
 	UINTN kernel_file_size = file_info->FileSize;
 
-	EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
+	VOID* kernel_buffer;
+	status = gBS->AllocatePool(EfiLoaderData, kernel_file_size, &kernel_buffer);
+	if (EFI_ERROR(status)) {
+		Print(u"Failed to allocate pool: %r\n", status);
+		Halt();
+	}
 
-	status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
+	status = kernel_file->Read(kernel_file, &kernel_file_size, kernel_buffer);
+	if (EFI_ERROR(status)) {
+		Print(u"Failed to read kernel: %r\n", status);
+		Halt();
+	}
+
+	Elf64_Ehdr* kernel_ehdr = (Elf64_Ehdr*)kernel_buffer;
+	UINT64 kernel_first_addr, kernel_last_addr;
+	CalcLoadAddressRange(kernel_ehdr, &kernel_first_addr, &kernel_last_addr);
+	UINTN num_pages = (kernel_last_addr - kernel_first_addr + 0xfff) / 0x1000;
+
+	status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, num_pages, &kernel_first_addr);
 	if (EFI_ERROR(status)) {
 		Print(u"Failed to allocate pages: %r\n", status);
 		Halt();
 	}
 
-	status = kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
+	CopyLoadSegments(kernel_ehdr);
+	Print(u"Kernel: 0x%0lx - 0x%0lx\n", kernel_first_addr, kernel_last_addr);
+
+	status = gBS->FreePool(kernel_buffer);
 	if (EFI_ERROR(status)) {
-		Print(u"Failed to read kernel: %r\n", status);
+		Print(u"Failed to free pool: %r\n", status);
 		Halt();
 	}
-	Print(u"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
 
 	// ブートサービスの終了
 	status = gBS->ExitBootServices(image_handle, memmap.map_key);
@@ -322,7 +372,7 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_tab
 
 	// カーネルの開始
 	// ELFファイルはエントリポイントアドレスがファイルの先頭から24バイト目から8バイト書かれる
-	UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
+	UINT64 entry_addr = *(UINT64*)(kernel_first_addr + 24);
 
 	typedef void EntryPointType(const struct FrameBufferConfig*);
 	EntryPointType* entry_point = (EntryPointType*)entry_addr;
