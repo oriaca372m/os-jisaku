35a29becc78abc8edabd2a0852ce1d644db2c01e
oriaca <oriaca372m@outlook.jp>
2021-06-01 17:50:20 +0900

    ブートローダーで色の書き込み

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,5 +1,5 @@
-#include "Uefi/UefiMultiPhase.h"
 #include <Guid/FileInfo.h>
+#include <Library/MemoryAllocationLib.h>
 #include <Library/PrintLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
@@ -130,6 +130,64 @@ static void Stop() {
 	}
 }
 
+static EFI_STATUS OpenGOP(EFI_HANDLE image_handle, EFI_GRAPHICS_OUTPUT_PROTOCOL** gop) {
+	UINTN num_gop_handles = 0;
+	EFI_HANDLE* gop_handles = NULL;
+	gBS->LocateHandleBuffer(ByProtocol, &gEfiGraphicsOutputProtocolGuid, NULL, &num_gop_handles, &gop_handles);
+
+	gBS->OpenProtocol(
+		gop_handles[0],
+		&gEfiGraphicsOutputProtocolGuid,
+		(VOID**)gop,
+		image_handle,
+		NULL,
+		EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+	FreePool(gop_handles);
+	return EFI_SUCCESS;
+}
+
+static const CHAR16* GetPixelFormatUnicode(EFI_GRAPHICS_PIXEL_FORMAT fmt) {
+	switch (fmt) {
+	case PixelRedGreenBlueReserved8BitPerColor:
+		return u"PixelRedGreenBlueReserved8BitPerColor";
+	case PixelBlueGreenRedReserved8BitPerColor:
+		return u"PixelBlueGreenRedReserved8BitPerColor";
+	case PixelBitMask:
+		return u"PixelBitMask";
+	case PixelBltOnly:
+		return u"PixelBltOnly";
+	case PixelFormatMax:
+		return u"PixelFormatMax";
+	default:
+		return u"InvalidPixelFormat";
+	}
+}
+
+static void DrawPixels(EFI_HANDLE image_handle) {
+	EFI_GRAPHICS_OUTPUT_PROTOCOL* gop;
+	OpenGOP(image_handle, &gop);
+
+	Print(
+		u"Resolution: %ux%u, Pixel Format: %s, %u pixels/line\n",
+		gop->Mode->Info->HorizontalResolution,
+		gop->Mode->Info->VerticalResolution,
+		GetPixelFormatUnicode(gop->Mode->Info->PixelFormat),
+		gop->Mode->Info->PixelsPerScanLine);
+
+	Print(
+		u"Frame Buffer: 0x%0lx - 0x%0lx, Size: %lu bytes\n",
+		gop->Mode->FrameBufferBase,
+		gop->Mode->FrameBufferBase + gop->Mode->FrameBufferSize,
+		gop->Mode->FrameBufferSize);
+
+	// 色の書き込み
+	UINT8* frame_buffer = (UINT8*)gop->Mode->FrameBufferBase;
+	for (UINTN i = 0; i < gop->Mode->FrameBufferSize; ++i) {
+		frame_buffer[i] = 0xff;
+	}
+}
+
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table) {
 	Print(u"Hello, world from Arch Linux!\n");
 
@@ -146,6 +204,8 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_tab
 
 	SaveMemoryMap(&memmap, memmap_file);
 	memmap_file->Close(memmap_file);
+
+	DrawPixels(image_handle);
 
 	// カーネルの読み込み
 	EFI_FILE_PROTOCOL* kernel_file;
