5064cb7d5635bddbe2229ff6139f8e3b81c08545
oriaca <oriaca372m@outlook.jp>
2021-06-02 00:41:16 +0900

    カーネルの関数名をスネークケースに

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -14,10 +14,10 @@ public:
 	PixelWriter(const FrameBufferConfig& config) : config_{config} {}
 	virtual ~PixelWriter() = default;
 
-	virtual void Write(int x, int y, const PixelColor& c) = 0;
+	virtual void write(int x, int y, const PixelColor& c) = 0;
 
 protected:
-	std::uint8_t* PixelAt(int x, int y) {
+	std::uint8_t* pixel_at(int x, int y) {
 		const int pixel_position = config_.pixels_per_scan_line * y + x;
 		return config_.frame_buffer + 4 * pixel_position;
 	}
@@ -29,8 +29,8 @@ private:
 class RGBResv8BitPerColorPixelWriter final : public PixelWriter {
 	using PixelWriter::PixelWriter;
 
-	void Write(int x, int y, const PixelColor& c) override {
-		auto p = PixelAt(x, y);
+	void write(int x, int y, const PixelColor& c) override {
+		auto p = pixel_at(x, y);
 		p[0] = c.r;
 		p[1] = c.g;
 		p[2] = c.b;
@@ -40,8 +40,8 @@ class RGBResv8BitPerColorPixelWriter final : public PixelWriter {
 class BGRResv8BitPerColorPixelWriter final : public PixelWriter {
 	using PixelWriter::PixelWriter;
 
-	void Write(int x, int y, const PixelColor& c) override {
-		auto p = PixelAt(x, y);
+	void write(int x, int y, const PixelColor& c) override {
+		auto p = pixel_at(x, y);
 		p[0] = c.b;
 		p[1] = c.g;
 		p[2] = c.r;
@@ -60,10 +60,10 @@ extern "C" void __cxa_pure_virtual() {
 	}
 }
 
-char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
-PixelWriter* pixel_writer = reinterpret_cast<PixelWriter*>(pixel_writer_buf);
-
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+	char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+	PixelWriter* pixel_writer = reinterpret_cast<PixelWriter*>(pixel_writer_buf);
+
 	if (frame_buffer_config.pixel_format == PixelFormat::RGBResv8BitPerColor) {
 		new (pixel_writer_buf) RGBResv8BitPerColorPixelWriter(frame_buffer_config);
 	} else if (frame_buffer_config.pixel_format == PixelFormat::BGRResv8BitPerColor) {
@@ -72,19 +72,13 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
 
 	for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
 		for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-			pixel_writer->Write(x, y, {0xFF, 0xFF, 0xFF});
+			pixel_writer->write(x, y, {0xFF, 0xFF, 0xFF});
 		}
 	}
 
 	for (int x = 0; x < 200; ++x) {
 		for (int y = 0; y < 100; ++y) {
-			pixel_writer->Write(x, y, {0x0, 0xFF, 0x0});
-		}
-	}
-
-	for (int x = 0; x < 200; ++x) {
-		for (int y = 100; y < 200; ++y) {
-			pixel_writer->Write(x, y, {0x0, 0x0, 0xFF});
+			pixel_writer->write(x, y, {0x0, 0xFF, 0x0});
 		}
 	}
 
