55b3f06c4f9b43c60d61f9e1ce124bc5724e8713
oriaca <oriaca372m@outlook.jp>
2021-06-02 15:02:03 +0900

    マウスの描画

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -32,6 +32,50 @@ int printk(const char* format, Args... args) {
 	return result;
 }
 
+namespace {
+	const int mouse_cursor_width = 15;
+	const int mouse_cursor_height = 24;
+	const char mouse_cursor_shape[mouse_cursor_height][mouse_cursor_width + 1] = {
+		"@              ", // keep shape
+		"@@             ", // keep shape
+		"@.@            ", // keep shape
+		"@..@           ", // keep shape
+		"@...@          ", // keep shape
+		"@....@         ", // keep shape
+		"@.....@        ", // keep shape
+		"@......@       ", // keep shape
+		"@.......@      ", // keep shape
+		"@........@     ", // keep shape
+		"@.........@    ", // keep shape
+		"@..........@   ", // keep shape
+		"@...........@  ", // keep shape
+		"@............@ ", // keep shape
+		"@......@@@@@@@@", // keep shape
+		"@......@       ", // keep shape
+		"@....@@.@      ", // keep shape
+		"@...@ @.@      ", // keep shape
+		"@..@   @.@     ", // keep shape
+		"@.@    @.@     ", // keep shape
+		"@@      @.@    ", // keep shape
+		"@       @.@    ", // keep shape
+		"         @.@   ", // keep shape
+		"         @@@   ", // keep shape
+	};
+
+	void draw_mouse(PixelWriter& writer, int x, int y) {
+		for (int dy = 0; dy < mouse_cursor_height; ++dy) {
+			for (int dx = 0; dx < mouse_cursor_width; ++dx) {
+				auto c = mouse_cursor_shape[dy][dx];
+				if (c == '@') {
+					writer.write(x + dx, y + dy, {0x00, 0x00, 0x00});
+				} else if (c == '.') {
+					writer.write(x + dx, y + dy, {0xFF, 0xFF, 0xFF});
+				}
+			}
+		}
+	}
+}
+
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
 	char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 	PixelWriter* pixel_writer = reinterpret_cast<PixelWriter*>(pixel_writer_buf);
@@ -48,13 +92,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
 	printk(u8"chino chan kawaii!\n");
 	printk(u8"gochuumon wa usagi desu ka?\n");
 
-	for (int i = 0; i < 100; i++) {
-		printk("i = %d", i);
-		printk(u8"chino chan kawaii!");
-
-		for (volatile int j = 0; j < 100000000; j++) {
-		}
-	}
+	draw_mouse(*pixel_writer, 200, 100);
 
 	while (true) {
 		__asm("hlt");
